29d268a5a3224128f3b1d9b438ac9cf5d0a25583
Sersawy <65075626+Abdelrhmansersawy@users.noreply.github.com>
2020-07-27 16:12:24 +0200

    'app'

---
+++ precache-manifest.83fd9cebfe63e417a1e2e281405f40a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0c381b1d7d4f34cf91ab71165b2e084",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a5755717c78bb34d22b",
+    "url": "/static/css/main.11c1f4fa.chunk.css"
+  },
+  {
+    "revision": "275ade9b55729c1cb4f6",
+    "url": "/static/js/2.9a77919a.chunk.js"
+  },
+  {
+    "revision": "43190b42fefe83e12b28e609399bf024",
+    "url": "/static/js/2.9a77919a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a5755717c78bb34d22b",
+    "url": "/static/js/main.95968d5e.chunk.js"
+  },
+  {
+    "revision": "58228b2e5bf98f14c815",
+    "url": "/static/js/runtime-main.dd3daeed.js"
+  }
+]);
